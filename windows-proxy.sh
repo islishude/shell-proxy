b5e78841087bf415da1882ba4d639d64d5728585
isLishude <lishude@aliyun.com>
2018-01-25 17:56:23 +0800

    feat: add windows proxy set

---
+++ windows-proxy.sh
@@ -0,0 +1,13 @@
+set -e
+echo "shell-proxy for Windows"
+echo "请输入代理地址"
+read url
+touch ~/.bash_profile
+echo "
+# 代理设置
+# github.com/lishude/shell-proxy
+export http_proxy=$url
+export https_proxy=$url
+export alias poff='unset http_proxy;unset https_proxy'" >> ~/.bash_profile
+#echo url >> $USER/.bashrc
+echo "配置代理成功，请退出当前 Shell，然后运行 test-proxy.sh 进行测试即可"
