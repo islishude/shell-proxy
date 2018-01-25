32ba913aae6c1b5b30b37470f28759ab8a650797
isLishude <lishude@aliyun.com>
2018-01-25 17:54:43 +0800

    fix: fix file name

--- add-proxy.sh
+++ add-proxy.sh
@@ -7,6 +7,6 @@ echo "
 # github.com/lishude/shell-proxy
 export http_proxy=$url
 export https_proxy=$url
-export alias poff='unset http_proxy;unset https_proxy'" >> ~/test.txt
+export alias poff='unset http_proxy;unset https_proxy'" >> ~/.bashrc
 #echo url >> $USER/.bashrc
 echo "配置代理成功，请退出当前 Shell，然后运行 test-proxy.sh 进行测试即可"
