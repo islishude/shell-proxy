96ed9c18321930b18823b86ae45393522a9c2d76
Shude Li <lishude@aliyun.com>
2019-03-27 06:56:19 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,3 +1,6 @@
-export url=http://127.0.0.1:1087
+# 代理设置
+purl=http://127.0.0.1:1080
+# 有时候 Shadowsock 会失效那么可以运行 poff 断开代理，然后重启 Shadowsock 即可
 alias poff='unset http_proxy;unset https_proxy'
-alias pon='export http_proxy=$url;export https_proxy=$url'
+# 运行 pon 即可快捷打开代理
+alias pon='export http_proxy=$purl; export https_proxy=$purl'
